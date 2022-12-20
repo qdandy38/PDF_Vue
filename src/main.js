99a770106e255cc35b105f8eb9ef7963756f0d7d
YunHsuanTsai <qdandy38@gmail.com>
2022-12-20 14:54:41 +0800

    feat pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from './App.vue'
 import router from "./router";
 import "./style/tailwind.css"
 import "./style/main.css";
 
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 
 app.mount('#app')
